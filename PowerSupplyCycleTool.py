97b89c2587e2d58ec948458c7afaf22d094651ab
federicopezzatopixsys <pixsys_44@pixsys.net>
2025-04-30 16:19:42 +0200

    move generic code into utils

--- PowerSupplyCycleTool.py
+++ PowerSupplyCycleTool.py
@@ -1,29 +1,19 @@
-import socket
 import tkinter as tk
-from tkinter import BooleanVar, IntVar, ttk, scrolledtext
 import threading
 import time
 import datetime
 import queue
 import subprocess
-import concurrent.futures
 from urllib3.exceptions import InsecureRequestWarning
 import requests
-import csv
 import os
-import sys
 import json
-from ipaddress import IPv4Address, ip_address
 from ordered_set import OrderedSet
-import io
-import git
-from paramiko import AuthenticationException, AutoAddPolicy, BadHostKeyException, SSHClient, SSHException
-from concurrent.futures import Future, ThreadPoolExecutor
-import re
-from pyModbusTCP.client import ModbusClient
 from config import TestBenchConfig
 from gui import FileFrame, InfoFrame, IpTableFrame, LogFrame, ManualControlsFrame, ModbusFrame, PsuFrame, SSHFrame, TimingFrame
 from enum import Enum
+from dp832 import dp832
+from utils import broadcast_modbus_read_poweron_counter, broadcast_modbus_read_register, broadcast_modbus_write_poweron_counter, broadcast_modbus_write_register, broadcast_ping, broadcast_ssh_command, get_current_git_commit_hash, ip_from_url, url_list_from_csv
 
 requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 
@@ -69,125 +59,6 @@ class Debouncer:
             self.root.after_cancel(self._job)
         self._job = self.root.after(self.delay, lambda: self.callback(*args, **kwargs))
 
-
-# Sostituisci con la tua implementazione o libreria effettiva per l'alimentatore Rigol.
-from dp832 import dp832
-
-def ip_from_url(url: str) -> (IPv4Address | None):
-    ip = None
-    ip_regex = r'\b(?:\d{1,3}\.){3}\d{1,3}\b'
-    ip_match = re.search(ip_regex, url)
-    if ip_match is not None:
-        ip = ip_match.group(0)
-        ip = IPv4Address(ip)
-    
-    return ip
-
-def get_current_git_commit_hash():
-    repo = git.Repo(os.getcwd())
-    sha = repo.head.object.hexsha
-    return sha
-
-def url_list_from_csv(content: str) -> OrderedSet[str]:
-    csv_file = io.StringIO(content)
-    csv_reader = csv.DictReader(csv_file, delimiter=';')
-    data = OrderedSet([row['url'] for row in csv_reader])
-    return data
-
-def run_ssh_command(server: IPv4Address, username: str, password: str, command: str) -> tuple:
-    ssh = SSHClient()
-    ssh.set_missing_host_key_policy(AutoAddPolicy())
-    ssh.connect(str(server), username=username, password=password)
-    _ = ssh.exec_command(command)
-    ssh.close()
-
-def run_modbus_read_registers(host: IPv4Address, reg_addr: int, reg_num: int, timeout: float):
-    c = ModbusClient(host=str(host), auto_open=True, auto_close=True, timeout=timeout)
-    regs = c.read_holding_registers(reg_addr, reg_num)
-    return regs
-
-def run_modbus_write_regiter(host: IPv4Address, reg_addr: int, value: int, timeout: float):
-    c = ModbusClient(host=str(host), auto_open=True, auto_close=True, timeout=timeout)
-    ok = c.write_single_register(reg_addr, value)
-    return ok
-
-# check if a given url returns HTTP code 200 (success) using curl
-# throws subprocess.TimeoutExpired or a generic exception
-def curl(url: str, timeout: float) -> (datetime.datetime | None):
-    result = subprocess.run(
-        ['curl', '-k', '-s', '-o', '/dev/null', '-w', '%{http_code}', url],
-        timeout=timeout,
-        capture_output=True,
-        text=True
-    )
-    timestamp = None
-    if result.stdout.strip() == '200':
-        timestamp = datetime.datetime.now()
-    
-    return timestamp
-
-# returns an dict where the key is an URL (string) and the value is its completed future
-# this way we can handle the future result outside of this function
-def broadcast_ping(url_list: set[str], timeout: float) -> dict[str, Future[datetime.datetime | None]]:
-    # spawn a bunch of workers to start the pinging process
-    future_results = dict()
-    with ThreadPoolExecutor(max_workers=20) as executor:
-        # run a different thread for each URL to ping and
-        # create a dictionary where the future is the key and the URL is the value
-        future_to_ip = {executor.submit(curl, url, timeout): url for url in url_list}
-        
-        # wait until every thread has finished, then iterate over each response and check it
-        for f in concurrent.futures.as_completed(future_to_ip.keys()):
-            ip = future_to_ip[f]
-            future_results[ip] = f
-    
-    return future_results
-
-# returns a dict where the key is the IP and the value is its completed future
-def broadcast_ssh_command(ip_list: set[IPv4Address], username: str, password: str, command: str) -> dict[IPv4Address, Future[None]]:
-    # spawn a bunch of workers to start the pinging process
-    future_results = dict()
-    with ThreadPoolExecutor(max_workers=20) as executor:
-        # run a different thread for each URL to ping and
-        # create a dictionary where the future is the key and the URL is the value
-        future_to_ip = {executor.submit(run_ssh_command, ip, username, password, command): ip for ip in ip_list}
-        
-        # wait until every thread has finished, then iterate over each response and check it
-        for f in concurrent.futures.as_completed(future_to_ip.keys()):
-            ip = future_to_ip[f]
-            future_results[ip] = f
-    
-    return future_results
-
-def broadcast_modbus_read_register(ip_list: set[IPv4Address], reg_addr: int, reg_num: int, timeout: float) -> dict[IPv4Address, Future[list | None]]:
-    future_results = dict()
-    with ThreadPoolExecutor(max_workers=20) as executor:
-        future_to_ip = {executor.submit(run_modbus_read_registers, ip, reg_addr, reg_num, timeout): ip for ip in ip_list}
-        
-        # wait until every thread has finished, then iterate over each response and check it
-        for f in concurrent.futures.as_completed(future_to_ip.keys()):
-            ip = future_to_ip[f]
-            future_results[ip] = f
-    
-    return future_results
-
-def broadcast_modbus_write_register(ip_list: set[IPv4Address], reg_addr: int, reg_value: int, timeout: float) -> dict[IPv4Address, Future[bool]]:
-    future_results = dict()
-    with ThreadPoolExecutor(max_workers=20) as executor:
-        future_to_ip = {executor.submit(run_modbus_write_regiter, ip, reg_addr, reg_value, timeout): ip for ip in ip_list}
-        
-        # wait until every thread has finished, then iterate over each response and check it
-        for f in concurrent.futures.as_completed(future_to_ip.keys()):
-            ip = future_to_ip[f]
-            future_results[ip] = f
-    
-    return future_results
-
-def broadcast_modbus_read_poweron_counter(ip_list: list[IPv4Address], timeout: float) -> dict[IPv4Address, Future[list | None]]:
-    return broadcast_modbus_read_register(ip_list, 0, 1, timeout)
-
-def broadcast_modbus_write_poweron_counter(ip_list: list[IPv4Address], reg_value: int, timeout: float) -> dict[IPv4Address, Future[bool]]:
-    return broadcast_modbus_write_register(ip_list, 0, reg_value, timeout)
 
 class RigolTestApp(tk.Tk):
     url_list_filename = 'urls.csv'
---
+++ utils.py
@@ -0,0 +1,130 @@
+import datetime
+import subprocess
+import concurrent.futures
+from urllib3.exceptions import InsecureRequestWarning
+import csv
+import os
+from ipaddress import IPv4Address
+from ordered_set import OrderedSet
+import io
+import git
+from paramiko import AutoAddPolicy, SSHClient
+from concurrent.futures import Future, ThreadPoolExecutor
+import re
+from pyModbusTCP.client import ModbusClient
+
+def ip_from_url(url: str) -> (IPv4Address | None):
+    ip = None
+    ip_regex = r'\b(?:\d{1,3}\.){3}\d{1,3}\b'
+    ip_match = re.search(ip_regex, url)
+    if ip_match is not None:
+        ip = ip_match.group(0)
+        ip = IPv4Address(ip)
+    
+    return ip
+
+def get_current_git_commit_hash():
+    repo = git.Repo(os.getcwd())
+    sha = repo.head.object.hexsha
+    return sha
+
+def url_list_from_csv(content: str) -> OrderedSet[str]:
+    csv_file = io.StringIO(content)
+    csv_reader = csv.DictReader(csv_file, delimiter=';')
+    data = OrderedSet([row['url'] for row in csv_reader])
+    return data
+
+def run_ssh_command(server: IPv4Address, username: str, password: str, command: str) -> tuple:
+    ssh = SSHClient()
+    ssh.set_missing_host_key_policy(AutoAddPolicy())
+    ssh.connect(str(server), username=username, password=password)
+    _ = ssh.exec_command(command)
+    ssh.close()
+
+def run_modbus_read_registers(host: IPv4Address, reg_addr: int, reg_num: int, timeout: float):
+    c = ModbusClient(host=str(host), auto_open=True, auto_close=True, timeout=timeout)
+    regs = c.read_holding_registers(reg_addr, reg_num)
+    return regs
+
+def run_modbus_write_regiter(host: IPv4Address, reg_addr: int, value: int, timeout: float):
+    c = ModbusClient(host=str(host), auto_open=True, auto_close=True, timeout=timeout)
+    ok = c.write_single_register(reg_addr, value)
+    return ok
+
+# check if a given url returns HTTP code 200 (success) using curl
+# throws subprocess.TimeoutExpired or a generic exception
+def curl(url: str, timeout: float) -> (datetime.datetime | None):
+    result = subprocess.run(
+        ['curl', '-k', '-s', '-o', '/dev/null', '-w', '%{http_code}', url],
+        timeout=timeout,
+        capture_output=True,
+        text=True
+    )
+    timestamp = None
+    if result.stdout.strip() == '200':
+        timestamp = datetime.datetime.now()
+    
+    return timestamp
+
+# returns an dict where the key is an URL (string) and the value is its completed future
+# this way we can handle the future result outside of this function
+def broadcast_ping(url_list: set[str], timeout: float) -> dict[str, Future[datetime.datetime | None]]:
+    # spawn a bunch of workers to start the pinging process
+    future_results = dict()
+    with ThreadPoolExecutor(max_workers=20) as executor:
+        # run a different thread for each URL to ping and
+        # create a dictionary where the future is the key and the URL is the value
+        future_to_ip = {executor.submit(curl, url, timeout): url for url in url_list}
+        
+        # wait until every thread has finished, then iterate over each response and check it
+        for f in concurrent.futures.as_completed(future_to_ip.keys()):
+            ip = future_to_ip[f]
+            future_results[ip] = f
+    
+    return future_results
+
+# returns a dict where the key is the IP and the value is its completed future
+def broadcast_ssh_command(ip_list: set[IPv4Address], username: str, password: str, command: str) -> dict[IPv4Address, Future[None]]:
+    # spawn a bunch of workers to start the pinging process
+    future_results = dict()
+    with ThreadPoolExecutor(max_workers=20) as executor:
+        # run a different thread for each URL to ping and
+        # create a dictionary where the future is the key and the URL is the value
+        future_to_ip = {executor.submit(run_ssh_command, ip, username, password, command): ip for ip in ip_list}
+        
+        # wait until every thread has finished, then iterate over each response and check it
+        for f in concurrent.futures.as_completed(future_to_ip.keys()):
+            ip = future_to_ip[f]
+            future_results[ip] = f
+    
+    return future_results
+
+def broadcast_modbus_read_register(ip_list: set[IPv4Address], reg_addr: int, reg_num: int, timeout: float) -> dict[IPv4Address, Future[list | None]]:
+    future_results = dict()
+    with ThreadPoolExecutor(max_workers=20) as executor:
+        future_to_ip = {executor.submit(run_modbus_read_registers, ip, reg_addr, reg_num, timeout): ip for ip in ip_list}
+        
+        # wait until every thread has finished, then iterate over each response and check it
+        for f in concurrent.futures.as_completed(future_to_ip.keys()):
+            ip = future_to_ip[f]
+            future_results[ip] = f
+    
+    return future_results
+
+def broadcast_modbus_write_register(ip_list: set[IPv4Address], reg_addr: int, reg_value: int, timeout: float) -> dict[IPv4Address, Future[bool]]:
+    future_results = dict()
+    with ThreadPoolExecutor(max_workers=20) as executor:
+        future_to_ip = {executor.submit(run_modbus_write_regiter, ip, reg_addr, reg_value, timeout): ip for ip in ip_list}
+        
+        # wait until every thread has finished, then iterate over each response and check it
+        for f in concurrent.futures.as_completed(future_to_ip.keys()):
+            ip = future_to_ip[f]
+            future_results[ip] = f
+    
+    return future_results
+
+def broadcast_modbus_read_poweron_counter(ip_list: list[IPv4Address], timeout: float) -> dict[IPv4Address, Future[list | None]]:
+    return broadcast_modbus_read_register(ip_list, 0, 1, timeout)
+
+def broadcast_modbus_write_poweron_counter(ip_list: list[IPv4Address], reg_value: int, timeout: float) -> dict[IPv4Address, Future[bool]]:
+    return broadcast_modbus_write_register(ip_list, 0, reg_value, timeout)
